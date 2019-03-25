bea4396baf0dcc749d87786cb9f58e0572325bc7
ray <r.qian@me.com>
2019-03-25 15:17:56 +0800

    init commit

---
+++ src/main/java/com/liberalisle/gpms/GpmsApplication.java
@@ -0,0 +1,13 @@
+package com.liberalisle.gpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpmsApplication.class, args);
+    }
+
+}
