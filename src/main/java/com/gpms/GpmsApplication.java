18c22a5c43d3f455849d98a95076e407a69552b9
Ray <r.qian@me.com>
2019-05-06 14:59:39 +0800

    修复目录结构改变后无法启动问题

--- src/main/java/com/gpms/GpmsApplication.java
+++ src/main/java/com/gpms/GpmsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.gpms.dao.mapper")
+@MapperScan(basePackages = "com.gpms.dao")
 public class GpmsApplication {
 
     public static void main(String[] args) {
